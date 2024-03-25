4d05e1fc0c45071e7f208a6ea0f188e17efce8e7
grigoriev.d.v <grig-dmitriy@mail.ru>
2024-03-25 15:06:38 +0300

    готовая версия приложения, минимально работоспособный прототип, без обработки выполнения со стороны adb

--- auto_install_apk_adb.py
+++ auto_install_apk_adb.py
@@ -2,11 +2,9 @@ import os
 import subprocess
 import customtkinter
 import threading
-import time
+from tkinter import filedialog
 
 empty_file = True
-apk_folder_path = 'C:/apk/app-RuPreprodFcm-debug-release-73.apk'
-apk_folder_path_alkor = "C:/Users/alkor/Desktop/auto_install_apk/apk/app-RuPreprodFcm-debug-develop-371.apk"
 app_name_adb_list_test_letu = "ru.letu.preprod"
 file_for_save_test_device_id = "test_device.txt"
 app_versions_in_device = "app_versions_in_device.txt"
@@ -45,42 +43,52 @@ def uninstall_app_to_all_devices():
 def install_apk_on_all_device():
     with open(file_for_save_test_device_id, "r") as file:
         devices = file.read().splitlines()
+    with open("user_apk_file.txt", "r") as apk_file:
+        apk_folder_path = apk_file.readline().strip()
     for device_id in devices:
         result = os.popen(f"adb -s {device_id} install {apk_folder_path}").read()
 
-def install_apk_on_all_device_async():
+def unitall_and_install_apk_on_all_device_async():
+    threading.Thread(target=scan_and_get_versions).start()
+    threading.Thread(target=uninstall_app_to_all_devices).start()
     threading.Thread(target=install_apk_on_all_device).start()
-
+   
 def scan_and_get_versions():
     scan_devices_id_and_safe_to_file()
     get_app_versions()
     read_file(file_for_save_test_device_id)
-    textbox.insert("0.0", device_list)
+
+def scan_devices_id_and_safe_to_file_cicle():
+    scan_devices_id_and_safe_to_file()
+    app.after(1000, scan_devices_id_and_safe_to_file_cicle)
 
 def read_file(filename):
     with open(filename, 'r') as file:
         text = file.read()
     return text
 
-device_list = read_file(file_for_save_test_device_id)
+def open_apk_file():
+    file_apk_path  = filedialog.askopenfilename()
+    with open('user_apk_file.txt', 'w') as file:
+        file.write(file_apk_path)
+
+def update_text_box():
+    device_list = read_file(file_for_save_test_device_id)
+    textbox = customtkinter.CTkTextbox(app)
+    textbox.delete("0.0", "end")
+    textbox.insert("0.0", device_list)
+    textbox.configure(state="disabled")  # configure textbox to be read-only
+    textbox.grid(row=0, column=1, padx=20, pady=20)
+    app.after(1000, update_text_box)
 
 app = customtkinter.CTk()
 app.title("APK Installer")
-app.geometry("250x450")
-
-textbox = customtkinter.CTkTextbox(app)
-textbox.insert("0.0", device_list)  # insert at line 0 character 0
-text = textbox.get("0.0", "end")  # get text from line 0 character 0 till the end
-textbox.configure(state="disabled")  # configure textbox to be read-only
-textbox.grid(row=0, column=1, padx=20, pady=20)
-
-button = customtkinter.CTkButton(app, text="Scan devices", command=scan_and_get_versions)
+app.geometry("240x400")
+app.resizable(False,False)
+scan_devices_id_and_safe_to_file_cicle()
+update_text_box()
+button = customtkinter.CTkButton(app, text="Open APK", command=open_apk_file, hover_color="green")
 button.grid(row=1, column=1, padx=20, pady=20)
-
-button = customtkinter.CTkButton(app, text="Uninstall for all", command=uninstall_app_to_all_devices)
+button = customtkinter.CTkButton(app, text="Global Install", command=unitall_and_install_apk_on_all_device_async, hover_color="red")
 button.grid(row=2, column=1, padx=20, pady=20)
-
-button = customtkinter.CTkButton(app, text="Global Install", command=install_apk_on_all_device_async)
-button.grid(row=3, column=1, padx=20, pady=20)
-
 app.mainloop()
