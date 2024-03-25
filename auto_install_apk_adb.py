112b42eca2897ec7125cf28c96da2f7b6fb4bc69
grigoriev.d.v <grig-dmitriy@mail.ru>
2024-03-25 10:31:10 +0300

    текстовый ферейм инициализирует текст внутри себя только при старте, надо исправлять. Программа закрывается без ошибок

--- auto_install_apk_adb.py
+++ auto_install_apk_adb.py
@@ -4,16 +4,12 @@ import customtkinter
 import threading
 import time
 
-
 empty_file = True
-apk_folder_path = 'C:/apk/3391.apk'
+apk_folder_path = 'C:/apk/app-RuPreprodFcm-debug-release-73.apk'
 apk_folder_path_alkor = "C:/Users/alkor/Desktop/auto_install_apk/apk/app-RuPreprodFcm-debug-develop-371.apk"
-interval = 1
 app_name_adb_list_test_letu = "ru.letu.preprod"
 file_for_save_test_device_id = "test_device.txt"
 app_versions_in_device = "app_versions_in_device.txt"
-
-
 
 def scan_devices_id_and_safe_to_file():
     result = os.popen("adb devices").read().split('\n')
@@ -46,9 +42,6 @@ def uninstall_app_to_all_devices():
                     package_name = line.split(':')[-1].strip()
                     subprocess.run(['adb', '-s', dev_id, 'uninstall', package_name], stdout=subprocess.PIPE)
 
-def install_apk_on_one_device(device_id):
-    result = os.popen(f"adb -s {device_id} install {apk_folder_path}").read()
-
 def install_apk_on_all_device():
     with open(file_for_save_test_device_id, "r") as file:
         devices = file.read().splitlines()
@@ -61,18 +54,33 @@ def install_apk_on_all_device_async():
 def scan_and_get_versions():
     scan_devices_id_and_safe_to_file()
     get_app_versions()
+    read_file(file_for_save_test_device_id)
+    textbox.insert("0.0", device_list)
+
+def read_file(filename):
+    with open(filename, 'r') as file:
+        text = file.read()
+    return text
+
+device_list = read_file(file_for_save_test_device_id)
 
 app = customtkinter.CTk()
-app.title("Broadcast APK")
-app.geometry("180x200")
+app.title("APK Installer")
+app.geometry("250x450")
+
+textbox = customtkinter.CTkTextbox(app)
+textbox.insert("0.0", device_list)  # insert at line 0 character 0
+text = textbox.get("0.0", "end")  # get text from line 0 character 0 till the end
+textbox.configure(state="disabled")  # configure textbox to be read-only
+textbox.grid(row=0, column=1, padx=20, pady=20)
 
 button = customtkinter.CTkButton(app, text="Scan devices", command=scan_and_get_versions)
-button.grid(row=0, column=1, padx=20, pady=20)
-
-button = customtkinter.CTkButton(app, text="Uninstall for all", command=uninstall_app_to_all_devices)
 button.grid(row=1, column=1, padx=20, pady=20)
 
-button = customtkinter.CTkButton(app, text="Global Install", command=install_apk_on_all_device_async)
+button = customtkinter.CTkButton(app, text="Uninstall for all", command=uninstall_app_to_all_devices)
 button.grid(row=2, column=1, padx=20, pady=20)
+
+button = customtkinter.CTkButton(app, text="Global Install", command=install_apk_on_all_device_async)
+button.grid(row=3, column=1, padx=20, pady=20)
 
 app.mainloop()
