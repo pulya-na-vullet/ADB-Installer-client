cdad0ffa3e8c041c6b5ecc342307626df4ab6ace
grigoriev.d.v <grig-dmitriy@mail.ru>
2024-03-27 10:39:48 +0300

    доработка выбора приложения через файл apk_to_unitall.txt, доработка текстового поля теперь под одним ID девайса отображаются все варианты и версии софта

--- APK Installer.py
+++ APK Installer.py
@@ -5,7 +5,6 @@ import threading
 from tkinter import filedialog
 
 empty_file = True
-app_name_adb_list_test_letu = "ru.letu.preprod"
 file_for_save_test_device_id = "test_device.txt"
 app_versions_in_device = "app_versions_in_device.txt"
 
@@ -19,26 +18,43 @@ def scan_devices_id_and_safe_to_file():
 def get_app_versions():
     with open(file_for_save_test_device_id, "r") as file:
         devices = file.read().splitlines()
+        
+    try:
+        with open("apk_to_unitall.txt", "r") as apk_file:
+            app_names = apk_file.read().splitlines()
+    except FileNotFoundError:
+        with open("apk_to_unitall.txt", "w") as new_apk_file:
+            new_apk_file.write("ru.letu.preprod\nru.letu")
+            app_names = ["ru.letu.preprod\nru.letu"]
+        
     with open(app_versions_in_device, "w") as output:
         for device_id in devices:
-            result = os.popen(f"adb -s {device_id} shell dumpsys package {app_name_adb_list_test_letu} | grep versionName").read()
-            if result:
-                version_name = result.split("=")[1].strip()
-                output.write(f"**{device_id}** : \nVersion = {version_name}\n")
-            else:
-                output.write(f"**{device_id}** : \nVersion = N/A\n")
+            output_data = {}
+            for app_name_adb_list_test_letu in app_names:
+                result = os.popen(f"adb -s {device_id} shell dumpsys package {app_name_adb_list_test_letu} | grep versionName").read()
+                if result:
+                    version_name = result.split("=")[1].strip()
+                    output_data[app_name_adb_list_test_letu] = version_name
+                else:
+                    output_data[app_name_adb_list_test_letu] = "N/A"
+            output.write(f"{device_id}\n")
+            for app_name, version in output_data.items():
+                output.write(f"{app_name} : {version}\n")
 
 def uninstall_app_to_all_devices():
-    with open(file_for_save_test_device_id, 'r') as file:
-        for line in file:
-            dev_id = line.strip()
-            result = subprocess.run(['adb', '-s', dev_id, 'shell', 'pm', 'list', 'packages', '-3'], stdout=subprocess.PIPE)
-            output = result.stdout.decode('utf-8')
-            lines = output.splitlines()
-            for line in lines:
-                if app_name_adb_list_test_letu in line:
-                    package_name = line.split(':')[-1].strip()
-                    subprocess.run(['adb', '-s', dev_id, 'uninstall', package_name], stdout=subprocess.PIPE)
+    with open('apk_to_unitall.txt', 'r') as file:
+        for app_name_adb_list_test_letu in file:
+            app_name_adb_list_test_letu = app_name_adb_list_test_letu.strip()
+            with open(file_for_save_test_device_id, 'r') as device_file:
+                for line in device_file:
+                    dev_id = line.strip()
+                    result = subprocess.run(['adb', '-s', dev_id, 'shell', 'pm', 'list', 'packages', '-3'], stdout=subprocess.PIPE)
+                    output = result.stdout.decode('utf-8')
+                    lines = output.splitlines()
+                    for line in lines:
+                        if app_name_adb_list_test_letu in line:
+                            package_name = line.split(':')[-1].strip()
+                            subprocess.run(['adb', '-s', dev_id, 'uninstall', package_name], stdout=subprocess.PIPE)
 
 def install_apk_on_all_device():
     with open(file_for_save_test_device_id, "r") as file:
@@ -58,10 +74,11 @@ def scan_and_get_versions():
     get_app_versions()
     read_file(file_for_save_test_device_id)
 
+# Очень дорогая функция из-за нее тормозит прил app.after(1000, scan_devices_id_and_safe_to_file_cicle)
 def scan_devices_id_and_safe_to_file_cicle():
     scan_devices_id_and_safe_to_file()
     get_app_versions()
-    app.after(1000, scan_devices_id_and_safe_to_file_cicle)
+    app.after(2000, scan_devices_id_and_safe_to_file_cicle)
 
 def read_file(filename):
     with open(filename, 'r') as file:
@@ -73,6 +90,7 @@ def open_apk_file():
     with open('user_apk_file.txt', 'w') as file:
         file.write(file_apk_path)
 
+# Очень дорогая функция из-за нее тормозит прил app.after(1000, update_text_box)
 def update_text_box():
     device_list = read_file(app_versions_in_device)
     textbox = customtkinter.CTkTextbox(app)
@@ -80,7 +98,7 @@ def update_text_box():
     textbox.insert("0.0", device_list)
     textbox.configure(state="disabled")  # configure textbox to be read-only
     textbox.grid(row=0, column=1, padx=20, pady=20)
-    app.after(1000, update_text_box)
+    app.after(2000, update_text_box)
 
 app = customtkinter.CTk()
 app.title("APK Installer")
